86d2d73d3c62283146f81700735b52d78a6bc510
admin <admin@5d7976f4-0b0e-0410-a586-d2bac8e3be52>
2000-12-27 14:30:01 +0000

    Include endian.h or machine/endian.h here.  Fix to work with header files that define BYTE_ORDER but not __BYTE_ORDER.
    git-svn-id: svn://svn.riscos.info/gccsdk/trunk@41 5d7976f4-0b0e-0410-a586-d2bac8e3be52

--- gcc/riscos-aof/as/endiandef.h
+++ gcc/riscos-aof/as/endiandef.h
@@ -4,13 +4,28 @@
  */
 #ifndef ENDIANDEF_H
 #define ENDIANDEF_H
-#include <endian.h>
 
+#ifdef HAVE_ENDIAN_H
+#include <endian.h>
+#elif HAVE_MACHINE_ENDIAN_H
+#include <machine/endian.h>
+#endif
+
+#ifdef __BYTE_ORDER
 #if __BYTE_ORDER == __LITTLE_ENDIAN
 #define armword(x) (x)
 #define ourword(x) (x)
 #else
 unsigned armword (unsigned val);
 unsigned ourword (unsigned val);
-#endif
+#endif /* __BYTE_ORDER != __LITTLE_ENDIAN */
+#elif BYTE_ORDER
+#define armword(x) (x)
+#define ourword(x) (x)
+#else
+unsigned armword (unsigned val);
+unsigned ourword (unsigned val);
+#endif /* BYTE_ORDER != LITTLE_ENDIAN */
+#endif /* BYTE_ORDER */
+
 #endif /* __ENDIANDEF_H */
