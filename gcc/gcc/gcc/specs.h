1e177ba069c5fc6c6eac5acafded8219e99642e8
admin <admin@5d7976f4-0b0e-0410-a586-d2bac8e3be52>
2000-12-27 09:47:49 +0000

    Include Objective-C specs.
    git-svn-id: svn://svn.riscos.info/gccsdk/trunk@22 5d7976f4-0b0e-0410-a586-d2bac8e3be52

--- gcc/gcc/gcc/specs.h
+++ gcc/gcc/gcc/specs.h
@@ -1,5 +1,5 @@
 #include "cp/lang-specs.h"
-/* #include "objc/lang-specs.h" */
+#include "objc/lang-specs.h"
 #include "f77/lang-specs.h"
 /* #include "ch/lang-specs.h" */
 #include "java/lang-specs.h"
