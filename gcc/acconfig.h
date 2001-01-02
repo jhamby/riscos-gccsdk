127eb87560b61412abc41b2d912b7b6f428fed06
admin <admin@5d7976f4-0b0e-0410-a586-d2bac8e3be52>
2001-01-02 13:02:48 +0000

    Add option --enable-filetype-ff8.
    git-svn-id: svn://svn.riscos.info/gccsdk/trunk@100 5d7976f4-0b0e-0410-a586-d2bac8e3be52

--- gcc/acconfig.h
+++ gcc/acconfig.h
@@ -1,4 +1,9 @@
+/* Support for additional configure directives.  */
+
 #undef PACKAGE
 #undef VERSION
 #undef HAVE_CPP_STRINGIFY
 
+/* Define if the linker should append `,ff8' to all executables.  This
+   will only occur in the cross-compiler.  */
+#undef ENABLE_FILETYPE_FF8
