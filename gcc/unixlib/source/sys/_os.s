c6a451326db927c054112c52c42fc82c9c09bd80
admin <admin@5d7976f4-0b0e-0410-a586-d2bac8e3be52>
2001-05-03 06:49:12 +0000

    Thu May  3 07:14:49 2001  Peter Naulls  <peter@erble.freeserve.co.uk>
    	* sys/_os.s (os_console): Don't set input stream to listen on
    	serial port.
    
    
    git-svn-id: svn://svn.riscos.info/gccsdk/trunk@139 5d7976f4-0b0e-0410-a586-d2bac8e3be52

--- gcc/unixlib/source/sys/_os.s
+++ gcc/unixlib/source/sys/_os.s
@@ -1,8 +1,8 @@
 ;----------------------------------------------------------------------------
 ;
-; $Source: /usr/local/cvsroot/unixlib/source/sys/s/_os,v $
-; $Date: 2000/12/21 15:09:13 $
-; $Revision: 1.15 $
+; $Source: /usr/local/cvsroot/gccsdk/unixlib/source/sys/_os.s,v $
+; $Date: 2001/01/29 15:10:21 $
+; $Revision: 1.2 $
 ; $State: Exp $
 ; $Author: admin $
 ;
@@ -14,15 +14,11 @@
 
 	EXPORT	os_console
 os_console
+	; Set operating system output stream to the console only
 	MOV	a1, #3
 	MOV	a2, #0
 	SWI	XOS_Byte
-	BVS	os_console_l1
-	MOV	a1, #2
-	MOV	a2, #2
-	SWI	XOS_Byte
 	MOVVC	a1, #0
-os_console_l1
 	MVNVS	a1, #0
 	return	AL, pc, lr
 
@@ -66,6 +62,7 @@ os_keyflush
 
 	EXPORT	os_423
 os_423
+	; Enable serial port and keyboard for input
 	MOV	a1, #2
 	MOV	a2, #2
 	SWI	XOS_Byte
