d0e25211f771fefb5f270e74b3f03a31a6728070
admin <admin@5d7976f4-0b0e-0410-a586-d2bac8e3be52>
2003-08-06 19:49:04 +0000

    (Makefile): Don't import the wide-char version of ctype.cc. (ctype.cc): Re-import.
    git-svn-id: svn://svn.riscos.info/gccsdk/trunk@736 5d7976f4-0b0e-0410-a586-d2bac8e3be52

--- gcc/gcc/libstdc/ctype.cc
+++ gcc/gcc/libstdc/ctype.cc
@@ -1,6 +1,5 @@
-// std::ctype implementation details, generic version -*- C++ -*-
-
-// Copyright (C) 2001, 2002 Free Software Foundation, Inc.
+// Copyright (C) 1997, 1998, 1999, 2000, 2001, 2002
+// Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
@@ -27,180 +26,123 @@
 // invalidate any other reasons why the executable file might be covered by
 // the GNU General Public License.
 
-//
-// ISO C++ 14882: 22.2.1.1.2  ctype virtual functions.
-//
-
-// Written by Benjamin Kosnik <bkoz@redhat.com>
-
 #include <locale>
 
-namespace std
+namespace std 
 {
-  // NB: The other ctype<char> specializations are in src/locale.cc and
-  // various /config/os/* files.
+  // XXX At some point, just rename this file to ctype_members_char.cc
+  // and compile it as a separate file instead of including it here.
+  // Platform-specific initialization code for ctype tables.
+  #include <bits/ctype_noninline.h>
+
+  // Definitions for locale::id of standard facets that are specialized.
+  locale::id ctype<char>::id;
+
+#ifdef _GLIBCPP_USE_WCHAR_T  
+  locale::id ctype<wchar_t>::id;
+#endif
+
   template<>
-    ctype_byname<char>::ctype_byname(const char* __s, size_t __refs)
-    : ctype<char>(0, false, __refs) 
+    const ctype<char>&
+    use_facet<ctype<char> >(const locale& __loc)
+    {
+      size_t __i = ctype<char>::id._M_id();
+      const locale::_Impl* __tmp = __loc._M_impl;
+      return static_cast<const ctype<char>&>(*(__tmp->_M_facets[__i]));
+    }
+
+#ifdef _GLIBCPP_USE_WCHAR_T
+  template<>
+    const ctype<wchar_t>&
+    use_facet<ctype<wchar_t> >(const locale& __loc)
+    {
+      size_t __i = ctype<wchar_t>::id._M_id();
+      const locale::_Impl* __tmp = __loc._M_impl;
+      return static_cast<const ctype<wchar_t>&>(*(__tmp->_M_facets[__i]));
+    }
+#endif
+
+  // Definitions for static const data members of ctype_base.
+  const ctype_base::mask ctype_base::space;
+  const ctype_base::mask ctype_base::print;
+  const ctype_base::mask ctype_base::cntrl;
+  const ctype_base::mask ctype_base::upper;
+  const ctype_base::mask ctype_base::lower;
+  const ctype_base::mask ctype_base::alpha;
+  const ctype_base::mask ctype_base::digit;
+  const ctype_base::mask ctype_base::punct;
+  const ctype_base::mask ctype_base::xdigit;
+  const ctype_base::mask ctype_base::alnum;
+  const ctype_base::mask ctype_base::graph;
+
+  const size_t ctype<char>::table_size;
+
+  ctype<char>::~ctype()
+  { 
+    _S_destroy_c_locale(_M_c_locale_ctype);
+    if (_M_del) 
+      delete[] this->table(); 
+  }
+
+  // These are dummy placeholders as these virtual functions are never called.
+  bool 
+  ctype<char>::do_is(mask, char_type) const 
+  { return false; }
+  
+  const char*
+  ctype<char>::do_is(const char_type* __c, const char_type*, mask*) const 
+  { return __c; }
+  
+  const char*
+  ctype<char>::do_scan_is(mask, const char_type* __c, const char_type*) const 
+  { return __c; }
+
+  const char* 
+  ctype<char>::do_scan_not(mask, const char_type* __c, const char_type*) const
+  { return __c; }
+
+  char
+  ctype<char>::do_widen(char __c) const
+  { return __c; }
+  
+  const char* 
+  ctype<char>::do_widen(const char* __lo, const char* __hi, char* __dest) const
+  {
+    memcpy(__dest, __lo, __hi - __lo);
+    return __hi;
+  }
+  
+  char
+  ctype<char>::do_narrow(char __c, char /*__dfault*/) const
+  { return __c; }
+  
+  const char* 
+  ctype<char>::do_narrow(const char* __lo, const char* __hi, 
+			 char /*__dfault*/, char* __dest) const
+  {
+    memcpy(__dest, __lo, __hi - __lo);
+    return __hi;
+  }
+
+#ifdef _GLIBCPP_USE_WCHAR_T
+  ctype<wchar_t>::ctype(size_t __refs) 
+  : __ctype_abstract_base<wchar_t>(__refs)
+  { _M_c_locale_ctype = _S_c_locale; }
+
+  ctype<wchar_t>::ctype(__c_locale __cloc, size_t __refs) 
+  : __ctype_abstract_base<wchar_t>(__refs) 
+  { _M_c_locale_ctype = _S_clone_c_locale(__cloc); }
+
+  ctype<wchar_t>::~ctype() 
+  { _S_destroy_c_locale(_M_c_locale_ctype); }
+
+  template<>
+    ctype_byname<wchar_t>::ctype_byname(const char* __s, size_t __refs)
+    : ctype<wchar_t>(__refs) 
     { 	
       _S_destroy_c_locale(_M_c_locale_ctype);
       _S_create_c_locale(_M_c_locale_ctype, __s); 
     }
+#endif
+} // namespace std
 
-#ifdef _GLIBCPP_USE_WCHAR_T  
-  ctype<wchar_t>::__wmask_type
-  ctype<wchar_t>::_M_convert_to_wmask(const mask __m) const
-  {
-    __wmask_type __ret;
-    switch (__m)
-      {
-      case space:
-	__ret = wctype("space");
-	break;
-      case print:
-	__ret = wctype("print");
-	break;
-      case cntrl:
-	__ret = wctype("cntrl");
-	break;
-      case upper:
-	__ret = wctype("upper");
-	break;
-      case lower:
-	__ret = wctype("lower");
-	break;
-      case alpha:
-	__ret = wctype("alpha");
-	break;
-      case digit:
-	__ret = wctype("digit");
-	break;
-      case punct:
-	__ret = wctype("punct");
-	break;
-      case xdigit:
-	__ret = wctype("xdigit");
-	break;
-      case alnum:
-	__ret = wctype("alnum");
-	break;
-      case graph:
-	__ret = wctype("graph");
-	break;
-      default:
-	__ret = 0;
-      }
-    return __ret;
-  };
-  
-  wchar_t
-  ctype<wchar_t>::do_toupper(wchar_t __c) const
-  { return towupper(__c); }
-
-  const wchar_t*
-  ctype<wchar_t>::do_toupper(wchar_t* __lo, const wchar_t* __hi) const
-  {
-    while (__lo < __hi)
-      {
-        *__lo = towupper(*__lo);
-        ++__lo;
-      }
-    return __hi;
-  }
-  
-  wchar_t
-  ctype<wchar_t>::do_tolower(wchar_t __c) const
-  { return towlower(__c); }
-  
-  const wchar_t*
-  ctype<wchar_t>::do_tolower(wchar_t* __lo, const wchar_t* __hi) const
-  {
-    while (__lo < __hi)
-      {
-        *__lo = towlower(*__lo);
-        ++__lo;
-      }
-    return __hi;
-  }
-
-  bool
-  ctype<wchar_t>::
-  do_is(mask __m, char_type __c) const
-  { return static_cast<bool>(iswctype(__c, _M_convert_to_wmask(__m))); }
-  
-  const wchar_t* 
-  ctype<wchar_t>::
-  do_is(const wchar_t* __lo, const wchar_t* __hi, mask* __m) const
-  {
-    while (__lo < __hi && !this->do_is(*__m, *__lo))
-      ++__lo;
-    return __lo;
-  }
-  
-  const wchar_t* 
-  ctype<wchar_t>::
-  do_scan_is(mask __m, const wchar_t* __lo, const wchar_t* __hi) const
-  {
-    while (__lo < __hi && !this->do_is(__m, *__lo))
-      ++__lo;
-    return __lo;
-  }
-
-  const wchar_t*
-  ctype<wchar_t>::
-  do_scan_not(mask __m, const char_type* __lo, const char_type* __hi) const
-  {
-    while (__lo < __hi && this->do_is(__m, *__lo) != 0)
-      ++__lo;
-    return __lo;
-  }
-
-  wchar_t
-  ctype<wchar_t>::
-  do_widen(char __c) const
-  { return btowc(__c); }
-  
-  const char* 
-  ctype<wchar_t>::
-  do_widen(const char* __lo, const char* __hi, wchar_t* __dest) const
-  {
-    mbstate_t __state;
-    memset(static_cast<void*>(&__state), 0, sizeof(mbstate_t));
-    mbsrtowcs(__dest, &__lo, __hi - __lo, &__state);
-    return __hi;
-  }
-
-  char
-  ctype<wchar_t>::
-  do_narrow(wchar_t __wc, char __dfault) const
-  { 
-    int __c = wctob(__wc);
-    return (__c == EOF ? __dfault : static_cast<char>(__c)); 
-  }
-
-  const wchar_t*
-  ctype<wchar_t>::
-  do_narrow(const wchar_t* __lo, const wchar_t* __hi, char __dfault, 
-	    char* __dest) const
-  {
-    size_t __offset = 0;
-    while (true)
-      {
-	const wchar_t* __start = __lo + __offset;        
-	size_t __len = __hi - __start;
-	
-	mbstate_t __state;
-	memset(static_cast<void*>(&__state), 0, sizeof(mbstate_t));
-	size_t __con = wcsrtombs(__dest + __offset, &__start, __len, &__state);
-	if (__con != __len && __start != 0)
-	  {
-	    __offset = __start - __lo;          
-	    __dest[__offset++] = __dfault;
-	  }
-	else
-	  break;
-      }
-    return __hi;
-  }
-#endif //  _GLIBCPP_USE_WCHAR_T
-}
