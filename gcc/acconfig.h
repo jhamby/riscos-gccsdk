d7a8661a9db7700095eed88de43e8b4683ac7ba2
nick <nick@5d7976f4-0b0e-0410-a586-d2bac8e3be52>
2004-04-17 15:45:34 +0000

    Convert configure.in scripts to autoconf 2.50 format.  This change gets rid of the WARNING messages that appear when running autoheader.
    Add rules to the toplevel configure.in and Makefile.in to auto-detect
    bison, flex, gperf, sed and makeinfo.  Fix all Makefiles that use
    these programs to use the macros instead.
    
    
    git-svn-id: svn://svn.riscos.info/gccsdk/trunk@967 5d7976f4-0b0e-0410-a586-d2bac8e3be52

--- gcc/acconfig.h
+++
@@ -1,9 +0,0 @@
-/* Support for additional configure directives.  */
-
-#undef PACKAGE
-#undef VERSION
-#undef HAVE_CPP_STRINGIFY
-
-/* Define if the linker should append `,ff8' to all executables.  This
-   will only occur in the cross-compiler.  */
-#undef ENABLE_FILETYPE_FF8
