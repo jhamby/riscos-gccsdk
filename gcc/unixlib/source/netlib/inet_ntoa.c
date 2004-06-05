3d6daade490016787058adb4be63cefc0976a220
joty <joty@5d7976f4-0b0e-0410-a586-d2bac8e3be52>
2004-06-05 19:53:23 +0000

    Sat Jun  5 21:49:35 2004  John Tytgat  <John.Tytgat@aaug.net>
    	* source/netlib/inet_ntoa.c (inet_ntoa): sprintf() used incorrect
    	  conversion specification.
    
    
    git-svn-id: svn://svn.riscos.info/gccsdk/trunk@1006 5d7976f4-0b0e-0410-a586-d2bac8e3be52

--- gcc/unixlib/source/netlib/inet_ntoa.c
+++ gcc/unixlib/source/netlib/inet_ntoa.c
@@ -1,15 +1,15 @@
 /****************************************************************************
  *
  * $Source: /usr/local/cvsroot/gccsdk/unixlib/source/netlib/inet_ntoa.c,v $
- * $Date: 2001/01/29 15:10:20 $
- * $Revision: 1.2 $
+ * $Date: 2003/12/29 19:02:39 $
+ * $Revision: 1.3 $
  * $State: Exp $
- * $Author: admin $
+ * $Author: peter $
  *
  ***************************************************************************/
 
 #ifdef EMBED_RCSID
-static const char rcs_id[] = "$Id: inet_ntoa.c,v 1.2 2001/01/29 15:10:20 admin Exp $";
+static const char rcs_id[] = "$Id: inet_ntoa.c,v 1.3 2003/12/29 19:02:39 peter Exp $";
 #endif
 
 #include <stdio.h>
@@ -26,7 +26,7 @@ inet_ntoa (struct in_addr in)
   static char string[16];
 
   /* Print the string into the buffer */
-  sprintf (string, "%uld.%uld.%uld.%uld", in.s_addr & 0xff,
+  sprintf (string, "%u.%u.%u.%u", in.s_addr & 0xff,
 	   (in.s_addr >> 8) & 0xff, (in.s_addr >> 16) & 0xff,
 	   (in.s_addr >> 24) & 0xff);
 
