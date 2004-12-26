cfbec48f0f2fc4211413f93507ee773845afda75
jmb <jmb@5d7976f4-0b0e-0410-a586-d2bac8e3be52>
2004-12-26 18:12:40 +0000

    Sun Dec 26 18:09:45 2004  John-Mark Bell <jmb202@ecs.soton.ac.uk>
    	* source/sys/_swi.s: Clean implementation of this file.
    
    
    git-svn-id: svn://svn.riscos.info/gccsdk/trunk@1158 5d7976f4-0b0e-0410-a586-d2bac8e3be52

--- gcc/unixlib/source/sys/_swi.s
+++ gcc/unixlib/source/sys/_swi.s
@@ -1,324 +1,255 @@
 ;----------------------------------------------------------------------------
 ;
-; $Source: /usr/local/cvsroot/gccsdk/unixlib/source/sys/_swi.s,v $
-; $Date: 2002/07/19 13:12:30 $
-; $Revision: 1.1.2.3 $
-; $State: Exp $
-; $Author: admin $
+; $Source$
+; $Date$
+; $Revision$
+; $State$
+; $Author$
+;
+; _swi and _swix implementation
 ;
 ;----------------------------------------------------------------------------
 
-	; This code is modified from the Straylight sources.
-	; Straylight is distributed under the GNU General Public License
+	AREA |C$$code|, CODE, READONLY
+
+	EXPORT _swi
+	EXPORT _swix
+
+	; _kernel_oserror *_swix(int swi_no, unsigned int, ...)
+_swix
+        ORR	a1, a1, #(1<<17)	; add X bit
 
 
-	GET	clib/unixlib/asm_dec.s
+	; int _swi(int swi_no, unsigned int, ...)
+_swi
+	STMFD	sp!, {a3, a4}		; push remaining arguments onto stack
 
-	AREA	|C$$code|, CODE, READONLY
-
-; --- _swi, _swix ---
-;
-; On entry:	r0 == SWI number
-;		r1 == feature flags:
-;			 0--9  == input registers
-;			  11   == local block flag
-;			12--15 == local block register
-;			16--19 == return register (_swi only)
-;			31--22 == output registers
-;		r2, r3 and stack contain other arguments
-;
-; On exit:	r0 == return value, or error indicator
-;
-; Use:		Calls a SWI or assembler routine.
-
-	; --- How this works ---
+	; order of variadic arguments on stack:
 	;
-	; The old version of this code used to build some neat code
-	; on the stack and then execute it.  This new spiffy version
-	; just saves data on the stack, because building code is a
-	; major no-no on the StrongARM.
+	; input regs (r0->r9) as specified by bits 0->9 of the mask
+	; output regs (r0->r9) as specified by bits 31->22 of the mask
+	; PC output reg, if mask bit 21 is set
+	; block arguments, if mask bit 11 is set
+
+
+	STMFD	sp!, {a2-a4, v1-v6, r10-r11, lr} ; save APCS regs & scratch
+	ADD	lr, sp, #48		; lr points to arguments
+
+	; scratch space on stack:
 	;
-	; The data stacked is as follows:
-	;
-	;	  pc   -- address to call
-	;	  r14  -- return address
-	;	  r12  -- saved r12 value
-	;	  r11  -- maybe Sapphire application context
-	;	  r10  -- maybe the SWI number
-	;
-	; Because I can't use dynamic code at all, I'm having to
-	; use some really nasty speed hacks here.
+	; sp+0	mask
+	; sp+4	pointer to remaining arguments (lr)
+	; sp+8	SWI number
 
-	; --- SWI entries ---
+	STR	a1, [sp, #8]		; save SWI num on stack
 
+	MOV	a3, #&300
+	ORR	a3, a3, #&FF
+	TST	a2, a3			; fast-track no arguments
+	BEQ	swi_block
 
-	EXPORT  |_swi|
-	NAME    _swi
-|_swi|
-	STMFD	sp!, {r2, r3}		;Stack all variable args
-	STMFD	sp!, {r4 - r12, r14}	;Save other registers
-	ADR	r12, |_swi_nonx|	;Point to non-X entry point
-	MOV	r9, r0			;Fetch the SWI number
-	LDR	r0, |_swihack|		;Point to SWI calling routine
-	B	|_swi_main|		;Skip onwards to main code
+	MOV	r11, a2			; stick mask in r11
 
-	EXPORT  |_swix|
-	NAME    _swix
-|_swix|
-	STMFD	sp!,{r2,r3}		;Stack all variable args
-	STMFD	sp!,{r4-r12,r14}	;Save other registers
-	BIC	r1,r1,#&000F0000	;Return r0 value no matter
-	ADR	r12,|_swi_x|		;Point to X entry point
-	ORR	r9,r0,#&20000		;Set the X bit on the SWI
-	LDR	r0,|_swihack|		;Point to SWI calling routine
+	; load registers off stack, two at a time
+	MOVS	r10, r11, LSL #31
+	LDRMI	a1, [lr], #4
+	LDRCS	a2, [lr], #4
+	MOVS	r10, r11, LSL #29
+	LDRMI	a3, [lr], #4
+	LDRCS	a4, [lr], #4
+	; done 4, can we stop here?
+	TST	r11, #&3F0
+	BEQ	swi_block
 
-	; --- First job is to set up the call address ---
+	MOVS	r10, r11, LSL #27
+	LDRMI	v1, [lr], #4
+	LDRCS	v2, [lr], #4
+	MOVS	r10, r11, LSL #25
+	LDRMI	v3, [lr], #4
+	LDRCS	v4, [lr], #4
+	MOVS	r10, r11, LSL #23
+	LDRMI	v5, [lr], #4
+	LDRCS	v6, [lr], #4
 
-|_swi_main|
-	MOV	r14,r0			;
-	SUB	sp,sp,#8		;Make a hole in the stack
-	STMFD	sp!,{r9-r12,r14}	;Save r10-r12 and pc (fake)
+	; now handle parameter block (if specified)
+swi_block
+	STR	lr, [sp, #4]		; save the argument ptr
 
-	; --- Set up the input registers ---
-	;
-	; Unrolled loop to do two registers at a time.  There are
-	; frequent exits while scanning the early registers to
-	; speed up common cases, petering out towards the end.
+	TST	r11, #(1<<11)
+	BEQ	swi_call
 
-	MOV	r10,r1			;Fetch the feature flags
-	ADD	r12,sp,#68		;Point to arguments
+	; count number of output regs, so we can point
+	; the block register somewhere sensible
+	MOV	r12, #0
+	MOVS	r10, r11, LSL #1
+	ADDMI	r12, r12, #1		; 1
+	ADDCS	r12, r12, #1		; 0
+	MOVS	r10, r11, LSL #3
+	ADDMI	r12, r12, #1		; 3
+	ADDCS	r12, r12, #1		; 2
+	MOVS	r10, r11, LSL #5
+	ADDMI	r12, r12, #1		; 5
+	ADDCS	r12, r12, #1		; 4
+	MOVS	r10, r11, LSL #7
+	ADDMI	r12, r12, #1		; 7
+	ADDCS	r12, r12, #1		; 6
+	MOVS	r10, r11, LSL #9
+	ADDMI	r12, r12, #1		; 9
+	ADDCS	r12, r12, #1		; 8
+	; return PC too?
+	TST	r11, #(1<<21)
+	ADDNE	r12, r12, #1
 
-	MOVS	r14,r10,LSL #31
-	LDRMI	r0,[r12],#4
-	LDRCS	r1,[r12],#4
-	TST	r10,#&3FC
-	BEQ	|done|
-	MOVS	r14,r10,LSL #29
-	LDRMI	r2,[r12],#4
-	LDRCS	r3,[r12],#4
-	TST	r10,#&3F0
-	BEQ	|done|
-	MOVS	r14,r10,LSL #27
-	LDRMI	r4,[r12],#4
-	LDRCS	r5,[r12],#4
-	TST	r10,#&3C0
-	BEQ	|done|
-	MOVS	r14,r10,LSL #25
-	LDRMI	r6,[r12],#4
-	LDRCS	r7,[r12],#4
-	MOVS	r14,r10,LSL #23
-	LDRMI	r8,[r12],#4
-	LDRCS	r9,[r12],#4
-|done|
-	; --- Now sort out block arguments ---
+	; block register number is in bits 12-15 of the mask
+	MOVS	r10, r11, LSL #17
+	BCC	%FA10
+	; 8, 9
+	TST	r11, #(1<<12)
+	ADDNE	v6, lr, r12, LSL #2	; 9
+	ADDEQ	v5, lr, r12, LSL #2	; 8
+	B	swi_call
+10	; 0->7
+	BPL	%FA20
+	; 4->7
+	TST	r11, #(3<<12)
+	ADDEQ	v1, lr, r12, LSL #2	; 4
+	BEQ	swi_call
 
-	ADD	r14,sp,#20		;Find the hole in the stack
-	STMIA	r14,{r10,r12}		;Save important context
-	TST	r10,#&800		;Do we have a block argument?
-	BNE	|_swi_block|		;Yes -- sort out out-of-line
-	LDMFD	sp!,{r10-r12,r14,pc}	;And call the routine
+	TSTNE	r11, #(1<<12)
+	ADDEQ	v3, lr, r12, LSL #2	; 6
+	BEQ	swi_call
 
-		; --- X-type return ---
+	TST	r11, #(1<<13)
+	ADDNE	v4, lr, r12, LSL #2	; 7
+	ADDEQ	v2, lr, r12, LSL #2	; 5
+	B	swi_call
+20	; 0->3
+	TST	r11, #(3<<12)
+	ADDEQ	a1, lr, r12, LSL #2	; 0
+	BEQ	swi_call
 
-|_swi_x|
-	LDMFD	sp!,{r10,r12}		;Reload important context
-	MOVVC	r14,#0			;If no error, return zero
-	MOVVS	r14,r0			;Otherwise point to the error
-	STR	r14,[sp,#-4]!		;Store as return value
-	B	|_swi_output|		;And skip on a little
+	TSTNE	r11, #(1<<12)
+	ADDEQ	a3, lr, r12, LSL #2	; 2
+	BEQ	swi_call
 
-	; --- Non-X-type return ---
-	;
-	; Pick out the correct register with a branch table.  Also
-	; invert the table to pick out common case of return r0.
+	TST	r11, #(1<<13)
+	ADDNE	a4, lr, r12, LSL #2	; 3
+	ADDEQ	a2, lr, r12, LSL #2	; 1
 
-|_swi_nonx|
-	LDMFD	sp!,{r10,r12}		;Reload important context
-	MOV	r14,#&F			;A nice bitmask
-	AND	r14,r14,r10,LSR #16	;So mask the return register
-	RSB	r14,r14,#&F		;Invert range hackily
-	ADD	pc,pc,r14,LSL #3	;And dispatch nicely
-	DCB	"hack"
+	; now call the SWI
+swi_call
+	LDR	r12, [sp, #8]		; grab the SWI num off the stack
+	STR	r11, [sp, #0]		; save the mask, for use on return
+	SWI	&71			; OS_CallASWIR12
 
-	STR	pc,[sp,#-4]!
-	B	|_swi_output|
-	STR	r14,[sp,#-4]!
-	B	|_swi_output|
-	STR	sp,[sp,#-4]!
-	B	|_swi_output|
-	STR	r12,[sp,#-4]!
-	B	|_swi_output|
-	STR	r11,[sp,#-4]!
-	B	|_swi_output|
-	STR	r10,[sp,#-4]!
-	B	|_swi_output|
-	STR	r9,[sp,#-4]!
-	B	|_swi_output|
-	STR	r8,[sp,#-4]!
-	B	|_swi_output|
-	STR	r7,[sp,#-4]!
-	B	|_swi_output|
-	STR	r6,[sp,#-4]!
-	B	|_swi_output|
-	STR	r5,[sp,#-4]!
-	B	|_swi_output|
-	STR	r4,[sp,#-4]!
-	B	|_swi_output|
-	STR	r3,[sp,#-4]!
-	B	|_swi_output|
-	STR	r2,[sp,#-4]!
-	B	|_swi_output|
-	STR	r1,[sp,#-4]!
-	B	|_swi_output|
-	STR	r0,[sp,#-4]!
+	; now handle the SWI return
+	LDR	lr, [sp, #4]		; restore argument pointer
+	LDR	r11, [sp, #0]		; restore mask
 
-	; --- Now handle output parameters ---
-	;
-	; Same style as the input parameters, with early exits
-	; placed conveniently.
-|_swi_output|
-	MRS	r11, CPSR		;Get the CPU flags
+	; TODO: should we return the entire PC, rather than just the PSR
+	;       (even though only the top 4 bits would have any meaning)?
+	MRS	r12, CPSR		; save flags (won't work on <ARMv3)
+	STR	r12, [sp, #0]		; on stack
 
-	TST	r10,#&FF000000		;Are there any output args?
-	TSTEQ	r10,#&00E00000
-	BEQ	swiout10		;No -- skip onwards then
+	BVC	fill_return		; V clear, continue as normal
+					; TODO: any way to avoid this branch?
 
-	MOVS	r14,r10,LSL #1
-	LDRCS	r14,[r12],#4
-	STRCS	r0,[r14,#0]
-	LDRMI	r14,[r12],#4
-	STRMI	r1,[r14,#0]
-	MOVS	r14,r10,LSL #3
-	LDRCS	r14,[r12],#4
-	STRCS	r2,[r14,#0]
-	LDRMI	r14,[r12],#4
-	STRMI	r3,[r14,#0]
-	TST	r10,#&0FC00000
-	BEQ	swiout00
-	MOVS	r14,r10,LSL #5
-	LDRCS	r14,[r12],#4
-	STRCS	r4,[r14,#0]
-	LDRMI	r14,[r12],#4
-	STRMI	r5,[r14,#0]
-	TST	r10,#&03C00000
-	BEQ	swiout00
-	MOVS	r14,r10,LSL #7
-	LDRCS	r14,[r12],#4
-	STRCS	r6,[r14,#0]
-	LDRMI	r14,[r12],#4
-	STRMI	r7,[r14,#0]
-	MOVS	r14,r10,LSL #9
-	LDRCS	r14,[r12],#4
-	STRCS	r8,[r14,#0]
-	LDRMI	r14,[r12],#4
-	STRMI	r9,[r14,#0]
-swiout00
-	; --- Handle returning flags ---
+	LDR	r12, [sp, #8]		; get the SWI num
+	TST	r12, #(1<<17)		; X bit set?
+	BNE	swi_end			; yes => exit
 
-	TST	r10,#&00200000
-	LDRNE	r14,[r12],#4
-	STRNE	r11,[r14,#0]
-swiout10
-	LDMFD	sp!,{r0,r4-r12,r14}
-	ADD	sp,sp,#8
-	MOV	pc,r14
+fill_return
+	; fill in return values
+	MOVS	r10, r11, LSL #1
+	LDRMI	r12, [lr], #4
+	STRMI	a2, [r12, #0]
+	LDRCS	r12, [lr], #4
+	STRCS	a1, [r12, #0]
+	MOVS	r10, r11, LSL #3
+	LDRMI	r12, [lr], #4
+	STRMI	a4, [r12, #0]
+	LDRCS	r12, [lr], #4
+	STRCS	a3, [r12, #0]
+	; done 4, can we stop here?
+	TST	r11, #&FC0
+	BEQ	swi_flags
 
-	; --- Handle block arguments ---
-	;
-	; Shift output registers to the right to find the block.
-	; Then dispatch through a branch table to store in the right
-	; register.  All the registers from r10 upwards are on the
-	; stack so they can be restored easily.
+	MOVS	r10, r11, LSL #5
+	LDRMI	r12, [lr], #4
+	STRMI	v2, [r12, #0]
+	LDRCS	r12, [lr], #4
+	STRCS	v1, [r12, #0]
+	MOVS	r10, r11, LSL #7
+	LDRMI	r12, [lr], #4
+	STRMI	v4, [r12, #0]
+	LDRCS	r12, [lr], #4
+	STRCS	v3, [r12, #0]
+	MOVS	r10, r11, LSL #9
+	LDRMI	r12, [lr], #4
+	STRMI	v6, [r12, #0]
+	LDRCS	r12, [lr], #4
+	STRCS	v5, [r12, #0]
 
-|_swi_block|
-	MOV	r11,r10,LSR #22		;Mask off output registers
-	MOV	r11,r11,LSL #21		;Shift down one place
-	MOV	r14,r12			;Preserve r12 here
-swibl00	MOVS	r11,r11,LSL #2		;Shift into C and N flags
-	ADDCS	r14,r14,#4		;If C set, bump counter
-	ADDMI	r14,r14,#4		;If N set, bump counter
-	BNE	swibl00			;And loop back until done
-	AND	r11,r10,#&0000F000	;Fetch the right argument
-	ADD	pc,pc,r11,LSR #9	;Dispatch through branch tbl
-	DCB	"hack"
+	; do we need to put the processor flags anywhere?
+swi_flags
+	TST	r11, #(1<<21)
+	LDRNE	lr, [lr, #0]
+	LDRNE	r12, [sp, #0]
+	STRNE	r12, [lr]
 
-	; --- Main dispatch table ---
-	;
-	; This is now just a branch off the main routine, so I
-	; can just call the SWI/routine appropriately rather than
-	; returning to the caller.  This gives me an extra register
-	; to play with above, which helps.
+swi_return
+	LDR	r12, [sp, #8]		; this is not applicable for swix
+	TST	r12, #(1<<17)
+	MOVNE	a1, #0			; ensure no error
+	BNE	swi_end
 
-	MOV	r0,r14
-	LDMFD	sp!,{r10-r12,r14,pc}
-	MOV	r1,r14
-	LDMFD	sp!,{r10-r12,r14,pc}
-	MOV	r2,r14
-	LDMFD	sp!,{r10-r12,r14,pc}
-	MOV	r3,r14
-	LDMFD	sp!,{r10-r12,r14,pc}
-	MOV	r4,r14
-	LDMFD	sp!,{r10-r12,r14,pc}
-	MOV	r5,r14
-	LDMFD	sp!,{r10-r12,r14,pc}
-	MOV	r6,r14
-	LDMFD	sp!,{r10-r12,r14,pc}
-	MOV	r7,r14
-	LDMFD	sp!,{r10-r12,r14,pc}
-	MOV	r8,r14
-	LDMFD	sp!,{r10-r12,r14,pc}
-	MOV	r9,r14
-	LDMFD	sp!,{r10-r12,r14,pc}
+	; return register number is in bits 16-19
+	; fast-track common case exit (r0)
+	TST	r11, #(15<<16)
+	BEQ	swi_end
 
-	; --- For safety, handle daft values of the parameter ---
+	; TODO: it's feasible to be asked to return r10->r14, too. should we?
+	MOVS	r10, r11, LSL #13
+	BCC	%FA30
+	; 8, 9, 15
+	TST	r11, #(1<<16)
+	MOVEQ	a1, v5			; 8
+	BEQ	swi_end
+	TSTNE	r11, #(1<<17)
+	MOVEQ	a1, v6			; 9
+	MOVNE	a1, r12			; 15
+	B	swi_end
+30	; 0->7
+	BPL	%FA40
+	; 4->7
+	TST	r11, #(3<<16)
+	MOVEQ	a1, v1			; 4
+	BEQ	swi_end
 
-	LDMFD	sp!,{r10-r12,r14,pc}
-	DCB	"daft"
-	LDMFD	sp!,{r10-r12,r14,pc}
-	DCB	"daft"
-	LDMFD	sp!,{r10-r12,r14,pc}
-	DCB	"daft"
-	LDMFD	sp!,{r10-r12,r14,pc}
-	DCB	"daft"
-	LDMFD	sp!,{r10-r12,r14,pc}
-	DCB	"daft"
-	LDMFD	sp!,{r10-r12,r14,pc}
-	DCB	"daft"
+	TSTNE	r11, #(1<<16)
+	MOVEQ	a1, v3			; 6
+	BEQ	swi_end
 
-|_swihack|
-	DCD	|__swihack|
+	TST	r11, #(1<<17)
+	MOVNE	a1, v4			; 7
+	MOVEQ	a1, v2			; 5
+	B	swi_end
+40	; 0->3
+	TST	r11, #(3<<16)
+	MOVEQ	a1, a1			; 0
+	BEQ	swi_end
 
-	; --- Initial sorting out and dispatching routine ---
+	TSTNE	r11, #(1<<16)
+	MOVEQ	a1, a3			; 2
+	BEQ	swi_end
 
-|__swihack|
-	STMFD	r13!,{r0,r1,r14}	;Store some registers away
-	MOV	r0,#0			;Read feature flags
-	SWI	XOS_PlatformFeatures	;Read the features then
-	ADRVC	r0,|_swihack_call|	;If SWI there, call directly
-	ADRVS	r0,|_swihack_build|	;Otherwise build the call
-	STR	r0,|_swihack|		;Store the value away
-	LDMFD	r13!,{r0,r1, r14}	;Restore the registers
-	LDR	pc,|_swihack|		;And snap the pointer
+	TST	r11, #(1<<17)
+	MOVNE	a1, a4			; 3
+	MOVEQ	a1, a2			; 1
 
-	; --- Dispatch to OS_CallASWI ---
-
-|_swihack_call|
-	MOV	r12,r14			;Save the return address
-	SWI	&6F			;OS_CallASWI
-|_ret_r12|
-	MOV	pc,r12			;And return to caller
-
-	; --- Dispatch by building code dynamically ---
-
-|_swihack_build|
-	STMFD	r13!,{r14}		;Save the return address
-	BIC	r10,r10,#&FF000000	;Clear the opcode byte
-	ORR	r10,r10,#&EF000000	;And make it SWIAL
-	LDR	r14,|_ret_r12|		;And load the return instr
-	STMFD	r13!,{r10,r14}		;Save code on the stack
-	MOV	r12,pc			;Set up return address
-	MOV	pc,r13			;And call the code
-	ADD	r13,r13,#8		;Reclaim stack space
-	LDMFD	r13!,{pc}		;And return with SWI's flags
+swi_end
+	LDMFD	sp!, {a2-a4, v1-v6, r10-r11, lr}
+	LDMFD	sp!, {a2, a3}
+	MOV	pc, lr
 
 	END
