17e366656952ce7946dc3105c592c43ea2ac4c42
alex <alex@5d7976f4-0b0e-0410-a586-d2bac8e3be52>
2005-01-09 23:29:04 +0000

    Sun Jan  9 23:26:02 2005  Alex Waugh  <alex@alexwaugh.com>
    	* source/locale/nl_langinfo.c (nl_langinfo): Change CODESET value to
    	  a format that iconv recognises.
    
    
    git-svn-id: svn://svn.riscos.info/gccsdk/trunk@1171 5d7976f4-0b0e-0410-a586-d2bac8e3be52

--- gcc/unixlib/source/locale/nl_langinfo.c
+++ gcc/unixlib/source/locale/nl_langinfo.c
@@ -1,15 +1,15 @@
 /****************************************************************************
  *
- * $Source: /usr/local/cvsroot/gccsdk/unixlib/source/locale/localeconv.c,v $
- * $Date: 2003/01/21 17:54:22 $
- * $Revision: 1.5 $
+ * $Source: /usr/local/cvsroot/gccsdk/unixlib/source/locale/nl_langinfo.c,v $
+ * $Date: 2004/09/17 18:39:54 $
+ * $Revision: 1.1 $
  * $State: Exp $
- * $Author: admin $
+ * $Author: peter $
  *
  ***************************************************************************/
 
 #ifdef EMBED_RCSID
-static const char rcs_id[] = "$Id: localeconv.c,v 1.5 2003/01/21 17:54:22 admin Exp $";
+static const char rcs_id[] = "$Id: nl_langinfo.c,v 1.1 2004/09/17 18:39:54 peter Exp $";
 #endif
 
 #include <locale.h>
@@ -24,7 +24,7 @@ nl_langinfo(nl_item item)
   switch (item)
   {
     case CODESET:
-      value = "8859-1";
+      value = "ISO-8859-1";
       break;
 
     default:
