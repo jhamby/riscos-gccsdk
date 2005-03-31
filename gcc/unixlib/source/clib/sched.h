dfd1f8ada3fd32df98eb95b94c6775d34fdcd39c
nick <nick@5d7976f4-0b0e-0410-a586-d2bac8e3be52>
2005-03-31 12:59:19 +0000

    Thu Mar 31 13:55:25 2005  Nick Burrett  <nick@sqrt.co.uk>
    	* sched.h (sched_yield): Add prototype.
    	* sched/syield.c: New file.
    
    	* Makefile.in (CROSS_CFLAGS): Set compiler to the C99 standard.
    
    
    git-svn-id: svn://svn.riscos.info/gccsdk/trunk@1262 5d7976f4-0b0e-0410-a586-d2bac8e3be52

--- gcc/unixlib/source/clib/sched.h
+++ gcc/unixlib/source/clib/sched.h
@@ -1,15 +1,21 @@
 /****************************************************************************
  *
- * $Source: $
- * $Date: $
- * $Revision: $
- * $State: $
- * $Author: $
+ * $Source: /usr/local/cvsroot/gccsdk/unixlib/source/clib/sched.h,v $
+ * $Date: 2002/12/15 13:16:55 $
+ * $Revision: 1.1 $
+ * $State: Exp $
+ * $Author: admin $
  *
  ***************************************************************************/
 
 #ifndef __SCHED_H
 #define __SCHED_H
+
+#ifndef __UNIXLIB_FEATURES
+#include <features.h>
+#endif
+
+__BEGIN_DECLS
 
 #define SCHED_FIFO 0
 #define SCHED_RR 1
@@ -20,6 +26,10 @@ struct sched_param
 {
   int sched_priority;
 };
+
+extern int sched_yield (void);
+
+__END_DECLS
 
 #endif /* __SCHED_H */
 
