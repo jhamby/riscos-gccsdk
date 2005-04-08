128a6dba035191522b06d1718217f592b843f0ab
nick <nick@5d7976f4-0b0e-0410-a586-d2bac8e3be52>
2005-04-08 19:22:12 +0000

    Fri Apr  8 20:20:41 2005  Nick Burrett  <nick@sqrt.co.uk>
    	* common/attrib.c (__set_protection): When translating,
    	take into account group ownerships.
    	(__get_protection): Likewise.
    
    
    git-svn-id: svn://svn.riscos.info/gccsdk/trunk@1302 5d7976f4-0b0e-0410-a586-d2bac8e3be52

--- gcc/unixlib/source/common/attrib.c
+++ gcc/unixlib/source/common/attrib.c
@@ -1,8 +1,8 @@
 /****************************************************************************
  *
- * $Source: /usr/local/cvsroot/unixlib/source/common/c/attrib,v $
- * $Date: 1999/11/16 13:26:49 $
- * $Revision: 1.5 $
+ * $Source: /usr/local/cvsroot/gccsdk/unixlib/source/common/attrib.c,v $
+ * $Date: 2001/01/29 15:10:19 $
+ * $Revision: 1.2 $
  * $State: Exp $
  * $Author: admin $
  *
@@ -10,7 +10,7 @@
  ***************************************************************************/
 
 #ifdef EMBED_RCSID
-static const char rcs_id[] = "$Id: attrib,v 1.5 1999/11/16 13:26:49 admin Exp $";
+static const char rcs_id[] = "$Id: attrib.c,v 1.2 2001/01/29 15:10:19 admin Exp $";
 #endif
 
 #include <sys/stat.h>
@@ -25,8 +25,9 @@ int __set_protection (__mode_t mode)
 
   attribute = (mode & S_IREAD) ? (1 << 0) : 0; /* Owner read */
   attribute |= (mode & S_IWRITE) ? (1 << 1) : 0; /* Owner write */
-  attribute |= (mode & S_IROTH) ? (1 << 4) : 0; /* Public read */
-  attribute |= (mode & S_IWOTH) ? (1 << 5) : 0; /* Public write */
+  attribute |= (mode & S_IEXEC) ? (1 << 2) : 0; /* Owner execute */
+  attribute |= ((mode & S_IROTH) || (mode & S_IRGRP)) ? (1 << 4) : 0; /* Public read */
+  attribute |= ((mode & S_IWOTH) || (mode & S_IWGRP)) ? (1 << 5) : 0; /* Public write */
   return attribute;
 }
 
@@ -36,9 +37,10 @@ __mode_t __get_protection (int attribute)
 {
   __mode_t mode;
 
-  mode = (attribute & (1 << 5)) ? S_IWOTH : 0; /* Public write */
-  mode |= (attribute & (1 << 0)) ? S_IREAD : 0; /* Owner read */
+  mode = (attribute & (1 << 0)) ? S_IREAD : 0; /* Owner read */
   mode |= (attribute & (1 << 1)) ? S_IWRITE : 0; /* Owner write */
-  mode |= (attribute & (1 << 4)) ? S_IROTH : 0; /* Public read */
+  mode |= (attribute & (1 << 2)) ? S_IEXEC : 0; /* Owner execute */
+  mode |= (attribute & (1 << 4)) ? (S_IROTH | S_IRGRP) : 0; /* Public read */
+  mode |= (attribute & (1 << 5)) ? (S_IWOTH | S_IWGRP) : 0; /* Public write */
   return mode;
 }
