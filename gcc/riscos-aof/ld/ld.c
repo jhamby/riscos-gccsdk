25d70740323e72832b22f9e124dfecd8da4ab15c
peter <peter@5d7976f4-0b0e-0410-a586-d2bac8e3be52>
2006-09-07 05:10:34 +0000

    Fix arithmetic
    git-svn-id: svn://svn.riscos.info/gccsdk/trunk@2238 5d7976f4-0b0e-0410-a586-d2bac8e3be52

--- gcc/riscos-aof/ld/ld.c
+++ gcc/riscos-aof/ld/ld.c
@@ -1720,8 +1720,8 @@ add_input_file (const char *fname)
     {
       size_t len = strlen(fname);
 
-      if (len >= sizeof("libX11.a")-1
-          && strcmp(fname + len - sizeof("libX11.a")-1, "libX11.a") == 0)
+      if (len >= sizeof("libX11.a") - 1
+          && strcmp(fname + len - (sizeof("libX11.a") - 1), "libX11.a") == 0)
         {
           add_chox11();
           return;
