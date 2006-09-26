0c1c91e3352b4041103f32c0585d16cad0737319
jmb <jmb@5d7976f4-0b0e-0410-a586-d2bac8e3be52>
2006-09-26 23:09:51 +0000

    Tue Sep 26 23:14:57 2006  John-Mark Bell  <jmb202@ecs.soton.ac.uk>
     * source/time/mktime.c: Fix corruption of input structure (bug #152)
    
    
    
    git-svn-id: svn://svn.riscos.info/gccsdk/trunk@2293 5d7976f4-0b0e-0410-a586-d2bac8e3be52

--- gcc/unixlib/source/time/mktime.c
+++ gcc/unixlib/source/time/mktime.c
@@ -12,6 +12,7 @@
 
 #include <locale.h>
 #include <time.h>
+#include <stddef.h>
 #include <string.h>
 #include <unixlib/os.h>
 #include <swis.h>
@@ -31,6 +32,13 @@ mktime (struct tm *brokentime)
   regs[1] = (int) riscos_time;
   regs[2] = (int) brokentime;
   __os_swi (Territory_ConvertTimeToOrdinals, regs);
+
+  /* Fixup normalised brokentime to lose centiseconds field */
+  memmove(brokentime, &brokentime->tm_min, offsetof(struct tm, tm_gmtoff));
+  /* And fixup month field */
+  brokentime->tm_mon -= 1;
+  /* And fixup year field */
+  brokentime->tm_year -= 1900;
 
   /* Set correct timezone information in brokentime structure.  */
   brokentime->tm_gmtoff = daylight * 3600 - timezone;
