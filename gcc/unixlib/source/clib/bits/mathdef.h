8fa821598906ca91481c47a24ae5ede0072fe2ed
joty <joty@5d7976f4-0b0e-0410-a586-d2bac8e3be52>
2006-12-16 15:13:33 +0000

    Sat Dec 16 14:41:51 CET 2006  John Tytgat  <John.Tytgat@aaug.net>
    	* source/clib/stddef.h: Removed.
    	* source/clib/bits/mathdef.h(INFINITY): Removed duplicate definition,
    	was also defined in source/clib/bits/inf.h.
    	This improves the gcc/g++ testsuite results.
    
    
    
    git-svn-id: svn://svn.riscos.info/gccsdk/trunk@2442 5d7976f4-0b0e-0410-a586-d2bac8e3be52

--- gcc/unixlib/source/clib/bits/mathdef.h
+++ gcc/unixlib/source/clib/bits/mathdef.h
@@ -29,10 +29,6 @@ typedef float float_t;		/* `float' expressions are evaluated as
 typedef double double_t;	/* `double' expressions are evaluated as
 				   `double'.  */
 
-/* Define `INFINITY' as value of type `float'.  */
-# define INFINITY	HUGE_VALF
-
-
 /* The values returned by `ilogb' for 0 and NaN respectively.  */
 # define FP_ILOGB0	(-2147483647)
 # define FP_ILOGBNAN	(2147483647)
