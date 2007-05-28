ad4b1099ef1e668ca9db051ea7545896b64d83dc
joty <joty@5d7976f4-0b0e-0410-a586-d2bac8e3be52>
2007-05-28 04:18:32 +0000

    Support for the libscl chunkstub split done in r2639 (allowing to omit chunkid 5 when not used).
    git-svn-id: svn://svn.riscos.info/gccsdk/trunk@2640 5d7976f4-0b0e-0410-a586-d2bac8e3be52

--- gcc4/recipe/files/ld/emulparams/armelf_riscos.sh
+++ gcc4/recipe/files/ld/emulparams/armelf_riscos.sh
@@ -3,6 +3,8 @@ SCRIPT_NAME=elf
 OUTPUT_FORMAT="elf32-littlearm"
 BIG_OUTPUT_FORMAT="elf32-bigarm"
 LITTLE_OUTPUT_FORMAT="elf32-littlearm"
+# On RISC OS it doesn't have any advantage to have page alignment so we
+# disable it by defining it to align on 4 bytes.
 MAXPAGESIZE=0x4
 COMMONPAGESIZE=0x4
 TEMPLATE_NAME=elf32
@@ -10,7 +12,7 @@ EXTRA_EM_FILE=armelf
 GENERATE_SHLIB_SCRIPT=yes
 
 DATA_START_SYMBOLS='__data_start = . ;';
-OTHER_TEXT_SECTIONS='*(.glue_7t) *(.glue_7)'
+OTHER_TEXT_SECTIONS='*(.glue_7t) *(.glue_7) *(.riscos.libscl.chunkstub.start) *(.riscos.libscl.chunkstub.id*) *(.riscos.libscl.chunkstub.end)'
 OTHER_BSS_SYMBOLS='__bss_start__ = .;'
 OTHER_BSS_END_SYMBOLS='_bss_end__ = . ; __bss_end__ = . ; __end__ = . ;'
 OTHER_SECTIONS='.note.gnu.arm.ident 0 : { KEEP (*(.note.gnu.arm.ident)) }'
