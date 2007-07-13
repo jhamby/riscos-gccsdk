a47dc3c47d99f9d82c5b5156ffe88fdcabd8f92b
lee <lee@5d7976f4-0b0e-0410-a586-d2bac8e3be52>
2007-07-13 19:10:00 +0000

    riscos/soloader/ln: 	Small utility to create the symlink files used by the dynamic 	linking system. Built for host and RISC OS as the build system 	needs it to create symlink files.
    riscos/soloader/config.mk:
    	Define HOST_GCC & LN.
    
    riscos/soloader/Makefile:
    	Add ln to list of things to build.
    	Expand dist target to include all libraries, create relevant
    	symlink files and zip it all up into an archive that can be
    	transfered to RISC OS. This assumes that GCCSDK is installed
    	and built which would have to be the case anyway to build the
    	dynamic linking system.
    
    
    git-svn-id: svn://svn.riscos.info/gccsdk/trunk@2701 5d7976f4-0b0e-0410-a586-d2bac8e3be52

--- gcc4/riscos/soloader/config.mk
+++ gcc4/riscos/soloader/config.mk
@@ -1,5 +1,7 @@
 CC = arm-unknown-riscos-gcc
 
+HOST_CC = gcc
+
 OBJCOPY = arm-unknown-riscos-objcopy
 
 AR = arm-unknown-riscos-ar
@@ -7,3 +9,5 @@ AR = arm-unknown-riscos-ar
 BUILDDIR = ../build
 
 ZIP = /home/riscos/cross/bin/zip
+
+LN = build/ln/host/ln
