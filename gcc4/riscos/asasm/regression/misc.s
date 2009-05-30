830dd666137482aae925606de23a5d1f3c96ed51
joty <joty@5d7976f4-0b0e-0410-a586-d2bac8e3be52>
2009-05-30 16:50:28 +0000

    Add legacy syntax for field specification for MSR/MRS.
    git-svn-id: svn://svn.riscos.info/gccsdk/trunk@3826 5d7976f4-0b0e-0410-a586-d2bac8e3be52

--- gcc4/riscos/asasm/regression/misc.s
+++ gcc4/riscos/asasm/regression/misc.s
@@ -12,6 +12,12 @@
 		MRS	R0,CPSR
 		MRS	R0,SPSR
 
+		; Legacy syntax:
+		MSR	CPSR,#&13	; control & flag
+		MSR	CPSR_all,#&13	; control & flag
+		MSR	CPSR_ctl,#&13	; control
+		MSR	CPSR_flg,#&13	; flg
+
 		MSR	CPSR_c,#&13
 		MSR	CPSR_cs,#&13
 		MSR	CPSR_csx,#&13
@@ -21,6 +27,12 @@
 		MSR	CPSR_cs,R0
 		MSR	CPSR_csx,R0
 		MSR	CPSR_csxf,R0
+
+		; Legacy syntax:
+		MSR	SPSR,#&13	; control & flag
+		MSR	SPSR_all,#&13	; control & flag
+		MSR	SPSR_ctl,#&13	; control
+		MSR	SPSR_flg,#&13	; flg
 
 		MSR	SPSR_c,#&13
 		MSR	SPSR_cs,#&13
@@ -45,6 +57,12 @@
 		DCD	&E10F0000
 		DCD	&E14F0000
 
+		; Legacy syntax:
+		DCD	&E329F013
+		DCD	&E329F013
+		DCD	&E321F013
+		DCD	&E328F013
+
 		DCD	&E321F013
 		DCD	&E325F013
 		DCD	&E327F013
@@ -54,6 +72,12 @@
 		DCD	&E125F000
 		DCD	&E127F000
 		DCD	&E12FF000
+
+		; Legacy:
+		DCD	&E369F013
+		DCD	&E369F013
+		DCD	&E361F013
+		DCD	&E368F013
 
 		DCD	&E361F013
 		DCD	&E365F013
