8f25429ca71531b6efa7716c2f41f001873baa09
lee <lee@5d7976f4-0b0e-0410-a586-d2bac8e3be52>
2009-06-14 14:19:43 +0000

    Sun Jun 14 14:46:12 2009  Lee Noar  <leenoar@sky.com>
    	* gcccompat/_eh_support.s:
    	* (__ehs_stack_limit): New function.
    	Given a stack pointer (which will not be the current one),
    	determine which stack chunk contains it and return the stack
    	limit for that chunk. Used by uw_install_context() in
    	unwind-dw2.c (call frame exception handling). The stack
    	unwinder steps through the call frames looking for either
    	cleanup handlers or exception handlers. The context is installed
    	once one of these is found, however, the frame that is jumped
    	to may be within a different stack chunk so we have to restore
    	the stack limit for that chunk.
    	* (__ehs_trim_stack): New function.
    	Wrapper for __trim_stack to preserve any registers that it may
    	use/corrupt. If the stack unwinder unwinds down the call frames
    	to a different stack chunk, then there will be one or more
    	redundant chunks that need to be freed.
    
    
    git-svn-id: svn://svn.riscos.info/gccsdk/trunk@3837 5d7976f4-0b0e-0410-a586-d2bac8e3be52

--- gcc4/recipe/files/libunixlib/gcccompat/_eh_support.s
+++ gcc4/recipe/files/libunixlib/gcccompat/_eh_support.s
@@ -6,6 +6,8 @@
 
 	.text
  
+#define STACKCHUNK_MAGIC_NUMBER 0xF60690FF
+
 	@ void *__ehs_return_address (void *fp)
 	@
 	@ Given a frame pointer, return real return address
@@ -13,41 +15,68 @@
 	NAME	__ehs_return_address
 __ehs_return_address:
 	MOV	ip, sp
-	STMFD	sp!, {fp, ip, lr, pc}
+	STMFD	sp!, {v1, fp, ip, lr, pc}
 	SUB	fp, ip, #4
 
-	MOVS	ip, a1
+	MOVS	v1, a1
 	MOVEQ	a1, #0
-	LDMEQEA	fp, {fp, sp, pc}
+	LDMEQEA	fp, {v1, fp, sp, pc}
 
 	@ Retrieve return address from stack frame
-	LDR	a1, [ip, #-4]
+	LDR	a1, [v1, #-4]
 	TEQ	a1, a1			@ 32bit mode check
 	TEQ	pc, pc
 	BICNE	a1, a1, #0xfc000003	@ If running 26bit, clear PSR bits.
 
 	LDR	lr, .L1			@ __gcc_alloca_free
 	TEQ	a1, lr
-	MOVEQ	a1, ip
+	MOVEQ	a1, v1
 	BLEQ	__gcc_alloca_return_address
 
 	@ SCL doesn't store the return address in the stack chunk
 #if __TARGET_UNIXLIB__
 	LDR	lr, .L2			@ __free_stack_chunk
 	TEQ	a1, lr
+	LDMNEEA	fp, {v1, fp, sp, pc}
+
+	@ Starting at the current chunk, step back through the chunk list
+	@ until we find the one containing the given frame pointer. The
+	@ return address we're looking for is in the _next_ chunk after
+	@ that one.
+	SUB	a2, sl, #512 + CHUNK_OVERHEAD
+	LDR	a4, =STACKCHUNK_MAGIC_NUMBER
+
+0:	@ chunk_loop
+	CMP	v1, a2
+	BLT	1f	@ next_chunk
+
+	LDR	a3, [a2, #CHUNK_SIZE]
+	ADD	a3, a2, a3
+
+	CMP	v1, a3
+	BLE	2f	@ found_chunk
+
+1:	@ next_chunk
+	LDR	a2, [a2, #CHUNK_PREV]
+	TEQ	a2, #0
+	MOVEQ	a1, #0
+	LDMEQEA	fp, {fp, sp, pc}
+
+	@ Make sure we've got a valid chunk
+	LDR	a3, [a2, #CHUNK_MAGIC]
+	TEQ	a3, a4
+	MOVNE	a1, #0
 	LDMNEEA	fp, {fp, sp, pc}
 
-	@ The logical thing to do here would be to find the stack chunk that
-	@ contains the fp arg and extract the return address from that as the
-	@ result. However, that doesn't work as far as C++ call frame
-	@ exception handling is concerned. Extracting the return address from
-	@ the current chunk does work.
+	B	0b	@ chunk_loop
 
-	SUB	a2, sl, #512 + CHUNK_OVERHEAD
-
-	LDR	a1, [a2, #CHUNK_RETURN]
+2:	@ found_chunk
+	LDR	a1, [a2, #CHUNK_NEXT]
+	TEQ	a1, #0
+	LDRNE	a1, [a1, #CHUNK_RETURN]
+	LDREQ	a1, [a2, #CHUNK_RETURN]
 #endif
-	LDMEA	fp, {fp, sp, pc}
+	LDMEA	fp, {v1, fp, sp, pc}
 .L1:
 	WORD	__gcc_alloca_free
 #if __TARGET_UNIXLIB__
@@ -55,5 +84,65 @@ __ehs_return_address:
 	WORD	__free_stack_chunk
 #endif
 	DECLARE_FUNCTION __ehs_return_address
+
+
+	@ void *__ehs_stack_limit (void *sp)
+	@
+	@ Given a stack pointer, return the stack limit of the chunk
+	@ that contains it. Used by uw_install_context() in unwind-dw2.c.
+	.global	__ehs_stack_limit
+	NAME	__ehs_stack_limit
+__ehs_stack_limit:
+	SUB	a2, sl, #512 + CHUNK_OVERHEAD
+	LDR	a4, =STACKCHUNK_MAGIC_NUMBER
+
+0:	@ chunk_loop
+	CMP	a1, a2
+	BLT	1f	@ next_chunk
+
+	LDR	a3, [a2, #CHUNK_SIZE]
+	ADD	a3, a2, a3
+
+	CMP	a1, a3
+	BLE	2f	@ found_chunk
+
+1:	@ next_chunk
+	LDR	a2, [a2, #CHUNK_PREV]
+	TEQ	a2, #0
+	MOVEQ	a1, #0
+	MOVEQ	pc, lr
+
+	@ Make sure we've got a valid chunk
+	LDR	a3, [a2, #CHUNK_MAGIC]
+	TEQ	a3, a4
+	MOVNE	a1, #0
+	MOVNE	pc, lr
+
+	B	0b	@ chunk_loop
+
+2:	@ found_chunk
+	ADD	a1, a2, #512 + CHUNK_OVERHEAD
+	MOV	pc, lr
+	DECLARE_FUNCTION __ehs_stack_limit
+
+
+	@ void __ehs_trim_stack (void)
+	@
+	@ Wrapper function for __trim_stack. Used by uw_install_context()
+	@ in unwind-dw2.c. The handler is stored at the sp of the context
+	@ that is to be returned to, then __builtin_eh_return restores
+	@ the registers for the context and jumps to __ehs_trim_stack.
+	.global	__ehs_trim_stack
+	NAME	__ehs_trim_stack
+__ehs_trim_stack:
+        @ Retrieve address of exception handler from stack
+	LDR	ip, [sp, #-4]
+
+	@ Save registers that __trim_stack may corrupt
+	STMFD	sp!, {a1-a4, ip}
+	BL	__trim_stack
+	LDMFD	sp!, {a1-a4, ip}
+	MOV	pc, ip
+	DECLARE_FUNCTION __ehs_trim_stack
 
 	.end
