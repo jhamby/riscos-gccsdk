9bef75953be1845e4060a4db092aa3fa15c69a0a
lee <lee@5d7976f4-0b0e-0410-a586-d2bac8e3be52>
2009-09-19 11:50:17 +0000

    Sat Sep 19 12:41:12 2009  Lee Noar  <leenoar@sky.com>
    	* gmon/gmon_start.c: Don't use _init symbol to find the
    	start of the text segment as this creates a bogus
    	DT_INIT entry in the dynamic segment. Instead use the
    	linker generated __text_start.
    
    
    git-svn-id: svn://svn.riscos.info/gccsdk/trunk@4131 5d7976f4-0b0e-0410-a586-d2bac8e3be52

--- gcc4/recipe/files/libunixlib/gmon/gmon-start.c
+++ gcc4/recipe/files/libunixlib/gmon/gmon-start.c
@@ -26,10 +26,10 @@
    as the external functions since we want the addresses of those
    labels. Taking the address of a function may have different
    meanings on different platforms. */
-extern void _init;
+extern void __text_start;
 extern void etext;
 
-#define TEXT_START &_init
+#define TEXT_START &__text_start
 
 /* In ELF and COFF, we cannot use the normal constructor mechanism to call
    __gmon_start__ because gcrt1.o appears before crtbegin.o in the link.
