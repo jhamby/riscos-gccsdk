a337ec2743cfc24dfa92fd987aac113731b35c72
peter <peter@5d7976f4-0b0e-0410-a586-d2bac8e3be52>
2010-01-07 22:03:11 +0000

    Thu Jan  7 13:47:15 PST 2010  Peter Naulls  <peter@chocky.org>
            * pwd/getpwnam_r.c(getpwnam_r): Return values weren't being properly
              set in default case.  Rewrite to do what man page says.
    
    
    
    git-svn-id: svn://svn.riscos.info/gccsdk/trunk@4425 5d7976f4-0b0e-0410-a586-d2bac8e3be52

--- gcc4/recipe/files/libunixlib/pwd/getpwnam_r.c
+++ gcc4/recipe/files/libunixlib/pwd/getpwnam_r.c
@@ -16,7 +16,7 @@
 /* Search for an entry with a matching name.  */
 int
 getpwnam_r (const char *name, struct passwd *result_buf, char *buffer,
-	    size_t buflen, struct passwd **result)
+            size_t buflen, struct passwd **result)
 {
   FILE *stream;
   struct passwd *p;
@@ -27,27 +27,29 @@ getpwnam_r (const char *name, struct passwd *result_buf, char *buffer,
     return __set_errno (EINVAL);
 
   stream = fopen ("/etc/passwd", "r");
+
   if (stream == NULL)
     {
-      *result = __pwddefault ();
-      return 0;
-    }
+      p = __pwddefault ();
 
-  p = __pwdread (stream, result_buf, buffer, buflen);
-  while (p != NULL)
+      if (result_buf) memcpy (result_buf, p, sizeof(struct passwd));
+    }
+  else
     {
-      if (strcmp (result_buf->pw_name, name) == 0)
-        break;
       p = __pwdread (stream, result_buf, buffer, buflen);
-    }
+      while (p != NULL)
+        {
+          if (strcmp (result_buf->pw_name, name) == 0)
+            break;
+          p = __pwdread (stream, result_buf, buffer, buflen);
+        }
 
-  fclose (stream);
+      fclose (stream);
+   }
 
-  if (p == NULL)
-    return -1;
-
-  if (result != NULL)
-    *result = result_buf;
+  if (p == NULL) return -1;
+  if (result != NULL) *result = result_buf;
+  if (buffer != NULL) strncpy (buffer, result ? (*result)->pw_passwd : p->pw_passwd, buflen);
 
   return 0;
 }
