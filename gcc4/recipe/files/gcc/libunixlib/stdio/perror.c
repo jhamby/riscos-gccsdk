d5144635873316f41cbfbad19c2764848d62d59a
joty <joty@5d7976f4-0b0e-0410-a586-d2bac8e3be52>
2011-03-12 14:04:07 +0000

    Sat Mar 12 15:04:27 CET 2011  John Tytgat  <John.Tytgat@aaug.net>
    	* stdio/perror.c (perror): Add check on empty string.
    
    
    git-svn-id: svn://svn.riscos.info/gccsdk/trunk@5074 5d7976f4-0b0e-0410-a586-d2bac8e3be52

--- gcc4/recipe/files/gcc/libunixlib/stdio/perror.c
+++ gcc4/recipe/files/gcc/libunixlib/stdio/perror.c
@@ -1,5 +1,5 @@
 /* perror ()
- * Copyright (c) 2000-2007 UnixLib Developers
+ * Copyright (c) 2000-2011 UnixLib Developers
  */
 
 #include <errno.h>
@@ -11,7 +11,7 @@ perror (const char *string)
 {
   const char *error = strerror (errno);
 
-  if (string)
+  if (string && *string)
     fprintf (stderr, "%s: %s\n", string, error);
   else
     fprintf (stderr, "%s\n", error);
