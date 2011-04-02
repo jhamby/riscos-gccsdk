b92b0e7366a413e8f11fa14e17f9652751a6bc5b
joty <joty@5d7976f4-0b0e-0410-a586-d2bac8e3be52>
2011-04-02 00:15:04 +0000

    Sat Apr  2 02:17:17 CEST 2011  John Tytgat  <John.Tytgat@aaug.net>
    	* time/_clock.s (clock): Removed.
    	* time/clock.c (clock): Add.
    	* Makefile.am: Follow changes.
    
    
    git-svn-id: svn://svn.riscos.info/gccsdk/trunk@5108 5d7976f4-0b0e-0410-a586-d2bac8e3be52

---
+++ gcc4/recipe/files/gcc/libunixlib/time/clock.c
@@ -0,0 +1,35 @@
+/* Determine processor time.
+   Copyright (c) 2011 UnixLib Developers.  */
+
+#include <errno.h>
+#include <time.h>
+
+#include <internal/os.h>
+
+static inline const _kernel_oserror *
+SWI_OS_ReadMonotonicTime (clock_t *resultp)
+{
+  register const _kernel_oserror *err __asm ("r0");
+  register clock_t result __asm ("r1");
+  __asm__ volatile ("SWI\t%[SWI_OS_ReadMonotonicTime]\n\t"
+		    "MOV\tr1, r0\n\t"
+		    "MOVVC\tr0, #0\n\t"
+		    : "=r" (err), "=r" (result)
+		    : [SWI_OS_ReadMonotonicTime] "i" (OS_ReadMonotonicTime | (1<<17))
+		    : "r14", "cc");
+  *resultp = result;
+  return err;
+}
+
+/* Return the elapsed processor time. The base time is arbitary, but doesn't
+   change.  */
+clock_t
+clock (void)
+{
+  clock_t result;
+  const _kernel_oserror *err;
+  if ((err = SWI_OS_ReadMonotonicTime (&result)) != NULL)
+    return __ul_seterr (err, ENOSYS);
+  return result;
+}
+
