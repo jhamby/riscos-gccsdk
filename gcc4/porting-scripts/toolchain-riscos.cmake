8f52f283d3e9f2e619d8c9ea07bca52433d65152
alanb <alanb@5d7976f4-0b0e-0410-a586-d2bac8e3be52>
2011-06-29 13:08:52 +0000

    Added toolchain file to use with cmake
    git-svn-id: svn://svn.riscos.info/gccsdk/trunk@5199 5d7976f4-0b0e-0410-a586-d2bac8e3be52

---
+++ gcc4/porting-scripts/toolchain-riscos.cmake
@@ -0,0 +1,17 @@
+# this one is important
+SET(CMAKE_SYSTEM_NAME GNU)
+#this one not so much
+SET(CMAKE_SYSTEM_VERSION 1)
+
+# specify the cross compiler
+SET(CMAKE_C_COMPILER   GCCSDK_BIN/arm-unknown-riscos-gcc.exe)
+SET(CMAKE_CXX_COMPILER GCCSDK_BIN/arm-unknown-riscos-g++.exe)
+
+# where is the target environment 
+SET(CMAKE_FIND_ROOT_PATH  GCCSDK_ENV)
+
+# search for programs in the build host directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+# for libraries and headers in the target directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
