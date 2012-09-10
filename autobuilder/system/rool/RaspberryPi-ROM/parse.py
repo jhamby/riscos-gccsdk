4f7740c84d664a5c5702501f1963644edb9d747a
caliston <caliston@5d7976f4-0b0e-0410-a586-d2bac8e3be52>
2012-09-10 16:57:58 +0000

    Add package scripts for Raspberry Pi ROM and firmware, scraping files off appropriate websites
    git-svn-id: svn://svn.riscos.info/gccsdk/trunk@5997 5d7976f4-0b0e-0410-a586-d2bac8e3be52

---
+++ autobuilder/system/rool/RaspberryPi-ROM/parse.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# horrifically insecure
+
+from BeautifulSoup import BeautifulSoup
+import urllib2
+import os
+url="http://www.riscosopen.org/content/downloads/other-zipfiles"
+page=urllib2.urlopen(url)
+soup = BeautifulSoup(page.read())
+#print soup
+for link in soup.findAll('a'): # find all links
+  if "BCM2835Dev.5.19.zip" in link['href']:
+    url=link['href']
+    
+url="http://www.riscosopen.org"+url
+timestamp=url.split('?')[1]
+archive=url.split('/')[-1]
+print "export ROOL_URL=\""+url+"\""
+print "export ROOL_ROM_UNIXSTAMP=\""+timestamp+"\""
+print "export ROOL_ROM_ARCHIVE=\""+archive+"\""
