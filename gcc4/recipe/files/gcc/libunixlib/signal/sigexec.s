cb00086dcd95d5b69d00ea7c3ae394e9ff05c919
joty <joty@5d7976f4-0b0e-0410-a586-d2bac8e3be52>
2012-12-01 18:03:20 +0000

    Sat Dec  1 18:52:54 CET 2012  John Tytgat  <John.Tytgat@aaug.net>
    	* signal/sigexec.s (__unixlib_exec_sig): Fix passing sighandler_t
    	arguments.
    
    
    git-svn-id: svn://svn.riscos.info/gccsdk/trunk@6198 5d7976f4-0b0e-0410-a586-d2bac8e3be52

--- gcc4/recipe/files/gcc/libunixlib/signal/sigexec.s
+++ gcc4/recipe/files/gcc/libunixlib/signal/sigexec.s
@@ -1,6 +1,6 @@
 @ __unixlib_exec_sig, __unixlib_exec_sigstack, __unixlib_exec_sigstack_bsd
 @ Written by Nick Burrett, 1 September 1996.
-@ Copyright (c) 1996-2008 UnixLib Developers
+@ Copyright (c) 1996-2012 UnixLib Developers
 
 #include "internal/asm_dec.s"
 
@@ -19,9 +19,9 @@ __unixlib_exec_sig:
 	SUB	fp, ip, #4
 	@ Call the signal handler
 	MOV	ip, a1
-	MOV	a3, a4
-	MOV	a2, a3
 	MOV	a1, a2
+	MOV	a2, a3
+	MOV	a3, a4
 	MOV	lr, pc
 	MOV	pc, ip
 	LDMEA	fp, {v1, v2, v3, v4, v5, v6, fp, sp, pc}
