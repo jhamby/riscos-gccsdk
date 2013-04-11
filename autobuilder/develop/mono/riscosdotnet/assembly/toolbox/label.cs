6c5ff6114f9eb8529ef77d9ee5f69def616ed5b7
lee <lee@5d7976f4-0b0e-0410-a586-d2bac8e3be52>
2013-04-11 16:31:01 +0000

    Missed files from previous commit:
    * assembly/toolbox/label.cs (class LabelTemplate): Add.
    * assembly/toolbox/labelled-box.cs (class LabelledBoxTemplate): Add.
    
    
    git-svn-id: svn://svn.riscos.info/gccsdk/trunk@6365 5d7976f4-0b0e-0410-a586-d2bac8e3be52

--- autobuilder/develop/mono/riscosdotnet/assembly/toolbox/label.cs
+++ autobuilder/develop/mono/riscosdotnet/assembly/toolbox/label.cs
@@ -23,5 +23,91 @@ namespace riscos
 			{
 			}
 		}
+
+		public static class LabelTemplateOffset
+		{
+			public const int Label = 36;
+			public const int TemplateSize = 40;
+		}
+
+		public static class LabelFlags
+		{
+			public const int NoBoundingBox = (1 << 0);
+			public const int Justify = (1 << 1);
+		}
+
+		public enum LabelJustify
+		{
+			Left,
+			Right,
+			Centre
+		}
+
+		/*! \brief Encapsulate the data required to create a Toolbox Label gadget
+		 * template.  */
+		public class LabelTemplate : GadgetTemplate
+		{
+			string _label = "";
+			//! \brief Get or set the text of the label.
+			public string Label {
+				get { return _label; }
+				set { _label = value; }
+			}
+
+			//! \brief Create a Toolbox Label gadget displaying the given text.
+			public LabelTemplate (string label) : base (Gadget.ComponentType.Label)
+			{
+				_label = label;
+			}
+
+			/*! \brief Set or get whether the label created from this template will display
+			 * a bounding box. It does by default.  */
+			public bool HasBoundingBox {
+				get { return (_flags & LabelFlags.NoBoundingBox) == 0; }
+				set {
+				      // Note the logic is reversed here because the flag is set
+				      // when there is no bounding box.
+				      _flags = (uint)(value ? _flags & ~LabelFlags.NoBoundingBox :
+							      _flags |  LabelFlags.NoBoundingBox);
+				}
+			}
+
+			/*! \brief Set or get the justification of the text in this label.  */
+			public LabelJustify Justify {
+				get { return (LabelJustify)((_flags >> LabelFlags.Justify) & 3); }
+				set {
+					_flags &= (3 << LabelFlags.Justify);
+					_flags |= (uint)value << LabelFlags.Justify;
+				}
+			}
+
+			public override int CalculateBufferSize (ref int strStart, ref int msgStart)
+			{
+				int size = base.CalculateBufferSize (ref strStart, ref msgStart);
+
+				size += _label.Length + 1;
+
+				return size;
+			}
+
+			public override void BuildBuffer (IntPtr buffer,
+							  int offset,
+							  ref int strOffset,
+							  ref int msgOffset)
+			{
+				base.BuildBuffer (buffer, offset, ref strOffset, ref msgOffset);
+
+				msgOffset = ObjectTemplate.WriteString (_label,
+									0,
+									buffer,
+									offset + LabelTemplateOffset.Label,
+									msgOffset);
+			}
+
+			public override int GetTemplateSize ()
+			{
+				return LabelTemplateOffset.TemplateSize;
+			}
+		}
 	}
 }
