206a7369e85e270181618c373d251ae48332b9a2
joty <joty@5d7976f4-0b0e-0410-a586-d2bac8e3be52>
2013-06-01 11:39:23 +0000

    Sat Jun  1 13:29:26 CEST 2013  John Tytgat  <John.Tytgat@aaug.net>
    	* configure.ac (AC_INIT): Increase version to 2.02.
    	* decode.c (decode): Fix NOINIT area + relocation case.
    	* *.c, *.h, COPYING: Update FSF address.
    
    
    git-svn-id: svn://svn.riscos.info/gccsdk/trunk@6444 5d7976f4-0b0e-0410-a586-d2bac8e3be52

--- tools/decaof/decode.c
+++ tools/decaof/decode.c
@@ -2,12 +2,12 @@
  * decode an AOF file
  *
  * Copyright (c) 1992 Andy Duplain, andy.duplain@dsl.pipex.com
- * Copyright (c) 2005-2012 GCCSDK Developers
+ * Copyright (c) 2005-2013 GCCSDK Developers
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,7 +16,8 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
  */
 
 #include "config.h"
@@ -205,9 +206,9 @@ decode (void)
 	{
 	  for (uint32_t areaIdx = 0, offset = 0; areaIdx != aofhdr->numareas; ++areaIdx)
 	    {
-	      print_area (ifp, &areahdrs[areaIdx], offset, offset + areahdrs[areaIdx].size);
-	      if (!(areahdrs[areaIdx].flags & AREA_UDATA))
-		offset += areahdrs[areaIdx].size + areahdrs[areaIdx].numrelocs*sizeof (struct reloc);
+	      Word relocOffset = (areahdrs[areaIdx].flags & AREA_UDATA) ? offset : offset + areahdrs[areaIdx].size;
+	      print_area (ifp, &areahdrs[areaIdx], offset, relocOffset);
+	      offset = relocOffset + areahdrs[areaIdx].numrelocs*sizeof (struct reloc);
 	    }
 	}
       
