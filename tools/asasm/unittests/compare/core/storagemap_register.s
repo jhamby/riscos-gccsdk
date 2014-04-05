0d57a94ed922c6472f8999f76da7b4944647ddb3
joty <joty@5d7976f4-0b0e-0410-a586-d2bac8e3be52>
2014-04-05 23:02:29 +0000

    Sun Apr  6 01:01:28 CEST 2014  John Tytgat  <John.Tytgat@aaug.net>
    	* unittests/compare/core/storagemap_register.s: Add BASED AREA test.
    
    
    git-svn-id: svn://svn.riscos.info/gccsdk/trunk@6641 5d7976f4-0b0e-0410-a586-d2bac8e3be52

--- tools/asasm/unittests/compare/core/storagemap_register.s
+++ tools/asasm/unittests/compare/core/storagemap_register.s
@@ -1,6 +1,7 @@
 ; Tests for ^, #, MAP, FIELD using register as base.
 
-	AREA	Code, CODE, READONLY
+	; Test 1:
+	AREA	Code1, CODE, READONLY
 
 		^	0, r12
 foo		#	4
@@ -13,7 +14,7 @@ sizeof_map	*	@
 		MOV	r1, #@
 		LDR	r2, baz
 		ADR	r3, bar
-		ADRL	r4, bar		; Will be detected ADR is sufficent.
+		ADRL	r4, bar		; Will detect ADR is sufficent.
 	|
 		LDR	r0,[r12,#4]
 		MOV	r1,#8
@@ -25,5 +26,46 @@ sizeof_map	*	@
 
 		MAP	0, r9
 baz		#	4
+
+	; Test 2:
+	AREA	Data2a, DATA, NOINIT, BASED r3
+		%	20
+tst2_a		%	32
+		^	tst2_a
+field0		#	4
+field1		#	8
+field2		#	16
+
+	AREA	Code2, CODE, READONLY
+	[ :LNOT: REFERENCE
+		LDR	r1, field1
+		LDR	r2, field4
+
+		LDR	r1, field1
+		LDR	r2, field4
+	|
+		LDR     r1, Data2a + 24
+		LDR     r2, Data2b + 24
+
+	  IF OUTPUTTYPE == "AOF"
+		LDR     r1, Data2a + 24
+		LDR     r2, Data2b + 24
+	  ELIF OUTPUTTYPE == "ELF"
+		LDR	r1, [r3, #20 + 4]
+		RELOC	R_ARM_LDR_SB_G0, Data2a
+		LDR	r2, [r3, #20 + 4]
+		RELOC	R_ARM_LDR_SB_G0, Data2b
+	  ELSE
+		Unsupported output type
+	  ENDIF
+	]
+
+	AREA	Data2b, DATA, NOINIT, BASED r3
+		%	20
+tst2_b		%	32
+		^	tst2_b
+field3		#	4
+field4		#	8
+field5		#	16
 
 	END
