e7889d767b09e6ec7e0447790133e97eef401a8e
joty <joty@5d7976f4-0b0e-0410-a586-d2bac8e3be52>
2014-05-04 13:36:49 +0000

    Sun May  4 15:35:43 CEST 2014  John Tytgat  <John.Tytgat@aaug.net>
    	* unittests/compare/core/def.s: Add disable & currently failing
    	test case.
    
    
    git-svn-id: svn://svn.riscos.info/gccsdk/trunk@6668 5d7976f4-0b0e-0410-a586-d2bac8e3be52

--- tools/asasm/unittests/compare/core/def.s
+++ tools/asasm/unittests/compare/core/def.s
@@ -1,6 +1,6 @@
 ; Tests the :DEF: operand
 
-	AREA	Code, CODE
+	AREA	Data, DATA
 
 	[ :LNOT: REFERENCE
 
@@ -12,21 +12,39 @@
 	= "$var is not defined\n"
 	]
 	MEND
-	
-	TestDef Foo	; Not defined
-	IMPORT	Bar
-	TestDef Bar	; Is defined
-	TestDef Code	; Is defined
-	GBLA	Bar2
-	TestDef Bar2	; Is defined
+
+	TestDef Foo		; Not defined
+	IMPORT	SymImp
+	TestDef SymImp		; Is defined
+	TestDef Data		; Is defined
+	EXPORT SymExp
+	TestDef SymExp
+SymExp
+	GBLL	VarLog
+	TestDef VarLog		; Is defined
+	GBLS	VarStr
+	TestDef VarStr		; Is defined
+	GBLA	VarArith
+	TestDef VarArith	; Is defined
+	[ {FALSE} ; FIXME: we should be able to enable this
+	TestDef |$$$$$$$|	; Not defined
+	]
 
 	|
 
 	= "Foo is not defined\n"
-	= "Bar is defined\n"
-	= "Code is defined\n"
-	= "Bar2 is defined\n"
+	= "SymImp is defined\n"
+	= "Data is defined\n"
+	EXPORT SymExp
+	= "SymExp is defined\n"
+SymExp
+	= "VarLog is defined\n"
+	= "VarStr is defined\n"
+	= "VarArith is defined\n"
+	[ {FALSE} ; FIXME: we should be able to enable this
+	= "|$$$$$$$| is not defined\n"
+	]
 
 	]
-	
+
 	END
