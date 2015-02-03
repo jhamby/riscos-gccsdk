934a85c8274e229a34f3b37e31bd17c41d94cf58
lee <lee@5d7976f4-0b0e-0410-a586-d2bac8e3be52>
2015-02-03 19:45:05 +0000

    Tue Feb 03 19:43:17 GMT  2015  Lee Noar  <leenoar@sky.com>
    	CLANG
    	* tools/clang/lib/Frontend/InitHeaderSearch.cpp(InitHeaderSearch::
    	AddDefaultCIncludePaths): RISC OS binary: Add C++ header path.
    
    
    git-svn-id: svn://svn.riscos.info/gccsdk/trunk@6767 5d7976f4-0b0e-0410-a586-d2bac8e3be52

--- autobuilder/develop/llvmclang/tools.clang.lib.Frontend.InitHeaderSearch.cpp.pp
+++ autobuilder/develop/llvmclang/tools.clang.lib.Frontend.InitHeaderSearch.cpp.pp
@@ -1,5 +1,5 @@
 --- tools/clang/lib/Frontend/InitHeaderSearch.cpp.orig	2014-06-10 22:07:12.000000000 +0100
-+++ tools/clang/lib/Frontend/InitHeaderSearch.cpp	2015-02-02 20:59:50.876396373 +0000
++++ tools/clang/lib/Frontend/InitHeaderSearch.cpp	2015-02-03 18:14:52.857468069 +0000
 @@ -231,6 +231,7 @@
      case llvm::Triple::NetBSD:
      case llvm::Triple::OpenBSD:
@@ -20,13 +20,18 @@
      for (SmallVectorImpl<StringRef>::iterator i = dirs.begin();
           i != dirs.end();
           ++i)
-@@ -451,6 +456,10 @@
+@@ -451,6 +456,15 @@
      AddGnuCPlusPlusIncludePaths("/opt/gcc4/include/c++/4.2.4",
                                  "i386-pc-solaris2.11", "", "", triple);
      break;
 +  case llvm::Triple::RISCOS:
++#ifdef __riscos__
++    AddGnuCPlusPlusIncludePaths("/GCCbin:../include/c++/4.7.4/",
++                                "arm-unknown-riscos", "", "", triple);
++#else
 +    AddGnuCPlusPlusIncludePaths("GCCSDK_CROSS_C++_HEADERS/",
 +                                "arm-unknown-riscos", "", "", triple);
++#endif
 +    break;
    default:
      break;
