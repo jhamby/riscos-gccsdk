125bb559ec2b44d34280b65757bd4c9585177bf5
lee <lee@5d7976f4-0b0e-0410-a586-d2bac8e3be52>
2020-11-22 13:35:17 +0000

    Sun Nov 22  13:26:08 GMT 2020 Lee Noar  <lee.noar@sky.com>
    	* include/paths.h: New header. This header is common on Unix systems,
    	and contains a reduced set of defines from other platforms that are
    	relevant to UnixLib.
    	* Makefile.am: Add new header.
    	Submitted by Cameron Cawley.
    
    
    git-svn-id: svn://svn.riscos.info/gccsdk/trunk@7444 5d7976f4-0b0e-0410-a586-d2bac8e3be52

---
+++ gcc4/recipe/files/gcc/libunixlib/include/paths.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2020 UnixLib Developers.
+ */
+
+#ifndef	_PATHS_H
+#define	_PATHS_H	1
+
+#ifndef __TARGET_SCL__
+
+#define _PATH_DEV     "/dev/"
+#define _PATH_CONSOLE "/dev/console"
+#define _PATH_DEVNULL "/dev/null"
+#define _PATH_RANDOM  "/dev/random"
+#define _PATH_TTY     "/dev/tty"
+#define _PATH_URANDOM "/dev/urandom"
+#define _PATH_DEVZERO "/dev/zero"
+
+#endif
+
+#endif
